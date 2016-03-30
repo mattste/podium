4b00401ce952fa698ddcccf888779398e33248c7
Matthew Stewart <mattdste@gmail.com>
2016-03-30 19:14:52 -0400

    hello world with dynamic templates

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -1,5 +1,7 @@
+from flask import render_template
+
 from . import main
 
 @main.route('/', methods=['GET'])
 def hello():
-	return "hello world!"
+	return render_template("hello.html", username="TheBigKahuna")
