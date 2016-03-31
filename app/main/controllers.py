d6feb59c30fac77b825814d2f44fd0a022aee1eb
Matthew Stewart <mattdste@gmail.com>
2016-03-30 20:15:14 -0400

    getting config

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import main
 
 @main.route('/', methods=['GET'])
 def hello():
-	return render_template("hello.html", username="TheBigKahuna")
+	current_db = current_app.config['RETHINKDB_DB']
+	return render_template("hello.html", username="TheBigKahuna", current_db=current_db)
