0edd4f7fc9438708049d34c727c6137e2299aa17
BuildTools <unconfigured@null.spigotmc.org>
2016-04-06 21:57:22 -0400

    Added more to controllers.py

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -18,46 +18,65 @@ def podiums():
 	current_db = current_app.config['RETHINKDB_DB']
 	return render_template("hello.html")
 
-mainTwilioNumber = '14243320631'
+mainTwilioNumber = '+14243320631'
 
 #Receive podium responses for main Podium Number
 @main.route('/podiumReceive', methods=['GET', 'POST'])
 def podiumReceive():
 	#Options for main Podium number
 		#Start: Gives "Welcome to Podium Message"
-		#Help: Displays "Help" message
 		#'Podium Name': Subscribes to specific podium
 		#Stop: Unsubscribes user from all podiums
 		#Else: Please send valid command to Podium
 
 	message = request.values.get('Body').lower();
 	fromNumber = request.values.get('From')
-	print(fromNumber)
+	toNumber = request.values.get('To')
+
+	#Endpoint to deal with texts to main twilio number
+	if(toNumber == mainTwilioNumber):
+		parseMainResponse(message, fromNumber)
+		pass
+	else:
+		pass
+		#Query dB for toNumber to see which podium responded to
+		#Create function to parse response from user
+
+	print(message)
+	return render_template("hello.html")
+
+def parseMainResponse(message, fromNumber):
 	start = re.search(r'start', message)
 	stop = re.search(r'stop', message)
+	twilioClient = TwilioActions()
 
 	#Get a list of all the podium handles
 	#podiumAccounts = getPodiumHandles()
 
 	#Loop thru podium handles and check if user's message matches existing account
 		#If we get a result, subscribe user to podium (add their number to specific podium account's subscriber list)
-		#subscribeName = 'name of account subscribing to'
+	subscribeName = 'name of account subscribing to'
 
 	if(start):
 		#Send start message
-		twilioClient = TwilioActions()
 		TwilioActions.podiumSendPollOrShout(twilioClient, "HOLA CHICO!", mainTwilioNumber, fromNumber)
 		subscribeUser(fromNumber, 'tutorial')
 		print("WOOO")
 	elif(subscribeName):
-		print("SUBSCRIBED")
+		pass
+		#ValidateSubscription(subscribeName, fromNumber)
+		#Subscribe(subscribeName, fromNumber)
+		#TwilioActions.podiumSendPollOrShout(twilioClient, "You have successfully subscribed", mainTwilioNumber, fromNumber)
 	elif(stop):
-		print("NOOOO")
+		unsubscribeAll(fromNumber)
+		TwilioActions.podiumSendPollOrShout(twilioClient, "You have successfully been unsubscribed from all Podium accounts.", mainTwilioNumber, fromNumber)
 	else:
-		print("Please send a valid command.")
+		TwilioActions.podiumSendPollOrShout(twilioClient, "Please send a valid command.", mainTwilioNumber, fromNumber)
 
-	print(message)
-	return render_template("hello.html")
+def parseResponse(message, fromNumber, toNumber):
+	#Check from toNumber which podium account they are talking to
+	#Get most recent poll and do appropriate action
+	pass
 
 def subscribeUser(fromNumber, podiumAccountName):
 	pass
@@ -66,6 +85,11 @@ def subscribeUser(fromNumber, podiumAccountName):
 def unsubscribeUser(fromNumber, podiumAccountName):
 	pass
 	#removeUserFromDatabase(fromNumber, podiumAccountName)
+
+def unsubscribeAll(fromNumber):
+	#loop thru all podium accounts
+		#call unsubscribeUser(fromNumber, podiumAccountName) for all accounts
+	pass
 
 @main.route('/test-podium', methods=['GET'])
 def testPodium():
